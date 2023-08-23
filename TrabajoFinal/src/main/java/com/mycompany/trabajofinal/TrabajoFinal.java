9f2b9777ecaa9ca840a9a40b5f78b4729af673f5
FaridHI <1121041@senati.pe>
2023-08-23 14:56:17 -0500

    llame al SCANNER para que trabaje

--- TrabajoFinal/src/main/java/com/mycompany/trabajofinal/TrabajoFinal.java
+++ TrabajoFinal/src/main/java/com/mycompany/trabajofinal/TrabajoFinal.java
@@ -11,6 +11,6 @@ import java.util.Scanner;
 public class TrabajoFinal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner (System.in);
     }
 }
